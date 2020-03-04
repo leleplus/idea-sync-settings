3a51d6e725115adef63b00835aac2bd596c1617d
logic <logic@DESKTOP-FD3DVSK>
2020-03-04 13:22:25 +0800

    IU-2019.3.3 <logic@DESKTOP-FD3DVSK Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, project.default.xml, rainbow_brackets.xml, Enum.java, Interface.java, lombok-plugin.xml, General Dark Structured.xml, terminal.xml, Default.xml, file.template.settings.xml, find.xml, ui.lnf.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, github.xml, mavenVersion.xml, studioFlags.xml, OGNL _Struts 2_.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, wsl.distributions.xml, MyBatisCodeHelper.xml, vcs.xml, databaseSettings.xml, OGNL.xml, editor.xml, debugger.xml, RecognEyes.icls, updates.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,14 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * Copyright (C) @${YEAR} fgwang.660@gmail.com
+ *
+ * @author lele
+ * @version 1.0
+ * @ClassName ${NAME}
+ * @date ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ * @description ${TODO}
+ */
+public enum ${NAME} {
+}
---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,14 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * Copyright (C) @${YEAR} fuguo.wang@logicsolutions.com.cn
+ *
+ * @author fuguo.wang
+ * @version 1.0
+ * @ClassName ${NAME}
+ * @date ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ * @description ${TODO}
+ */
+public interface ${NAME} {
+}
